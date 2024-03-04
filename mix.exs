8a038ea9324d0266da48fb980f10762a65d1945a
Jason Garber <jason.garber@cargosense.com>
2024-03-04 10:24:11 -0500

    Add CI workflow and example Mix file

---
+++ mix.exs
@@ -0,0 +1,18 @@
+# Used for testing
+defmodule App.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :app,
+      version: "1.0.0",
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    [
+      {:jason, "~> 1.0"}
+    ]
+  end
+end
